745b93948f3fa2f6f00c2e45530eea808904f3b7
Marcus Vinícius <marcusvinicius@Marcuss-iMac.local>
2016-09-27 00:31:15 -0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Marcus Vinícius on 9/19/16.
+//  Copyright © 2016 Marcus Vinícius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    @IBAction func touchDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+        }
+        
+        userIsInTheMiddleOfTyping = true
+    }
+    
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    @IBAction func performOperation(sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            if mathematicalSymbol == "pi" {
+                displayValue = M_PI
+            }
+        }
+    }
+}
